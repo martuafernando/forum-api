66933ebafe95007568918312579f3125b140a255
martuafernando <fernandosibarani45@gmail.com>
2023-07-23 11:04:36 +0700

    test(thread): add test for thread api

--- src/Infrastructures/http/_test/threads.test.js
+++ src/Infrastructures/http/_test/threads.test.js
@@ -3,6 +3,7 @@ const ThreadsTableTestHelper = require('../../../../tests/ThreadsTableTestHelper
 const container = require('../../container');
 const createServer = require('../createServer');
 const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const UserApiTestHelper = require('../../../../tests/UsersApiTestHelper');
 
 describe('/threads endpoint', () => {
   afterEach(async () => {
@@ -21,26 +22,7 @@ describe('/threads endpoint', () => {
     };
     const server = await createServer(container);
 
-    // Add user
-    await server.inject({
-      method: 'POST',
-      url: '/users',
-      payload: {
-        username: 'dicoding',
-        password: 'secret',
-        fullname: 'Dicoding Indonesia',
-      },
-    });
-    // Login user
-    const loginResponse = await server.inject({
-      method: 'POST',
-      url: '/authentications',
-      payload: {
-        username: 'dicoding',
-        password: 'secret',
-      },
-    });
-    const { data: { accessToken } } = JSON.parse(loginResponse.payload);
+    const accessToken = await UserApiTestHelper.getAccessTokenFromUser({})
 
     // Action
     const response = await server.inject({
@@ -57,5 +39,53 @@ describe('/threads endpoint', () => {
     expect(response.statusCode).toEqual(400);
     expect(responseJson.status).toEqual('fail');
     expect(responseJson.message).toEqual('tidak dapat membuat thread baru karena properti yang dibutuhkan tidak ada');
+  });
+
+  it('should response 401 when request payload not authorization header', async () => {
+    // Arrange
+    const requestPayload = {
+      title: 'thread-title',
+      body: 'thread-body',
+    };
+    const server = await createServer(container);
+
+    // Action
+    const response = await server.inject({
+      method: 'POST',
+      url: '/threads',
+      payload: requestPayload,
+    });
+
+    // Assert
+    const responseJson = JSON.parse(response.payload);
+    expect(response.statusCode).toEqual(401);
+    expect(responseJson.status).toEqual('fail');
+    expect(responseJson.message).toEqual('Missing authentication');
+  });
+
+  it('should response 201 when thread created', async () => {
+    // Arrange
+    const requestPayload = {
+      title: 'thread-title',
+      body: 'thread-body',
+    };
+    const server = await createServer(container);
+
+    const accessToken = await UserApiTestHelper.getAccessTokenFromUser({})
+
+    // Action
+    const response = await server.inject({
+      method: 'POST',
+      url: '/threads',
+      headers: {
+        authorization: `Bearer ${accessToken}`
+      },
+      payload: requestPayload,
+    });
+
+    // Assert
+    const responseJson = JSON.parse(response.payload);
+    expect(response.statusCode).toEqual(201);
+    expect(responseJson.status).toEqual('success');
   });
 });
---
+++ tests/UsersApiTestHelper.js
@@ -0,0 +1,36 @@
+const container = require('../src/Infrastructures/container');
+const createServer = require('../src/Infrastructures/http/createServer');
+
+const UserApiTestHelper = {
+  async getAccessTokenFromUser({
+    username = 'dicoding',
+    password = 'secret',
+    fullname = 'Dicoding Indonesia',
+  }) {
+    const server = await createServer(container);
+
+    // Add user
+    await server.inject({
+      method: 'POST',
+      url: '/users',
+      payload: {
+        username,
+        password,
+        fullname,
+      },
+    });
+    // Login user
+    const loginResponse = await server.inject({
+      method: 'POST',
+      url: '/authentications',
+      payload: {
+        username,
+        password,
+      },
+    });
+    const { data: { accessToken } } = JSON.parse(loginResponse.payload);
+    return accessToken
+  }
+}
+
+module.exports = UserApiTestHelper
